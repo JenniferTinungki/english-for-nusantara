f7624e6d005feaaf700d9043cbaf8a2e4b342be3
JenniferTinungki <jennifertinungki@gmail.com>
2026-05-04 01:52:05 +0800

    fix AssessmentSeeder dan TugasSeeder

--- database/seeders/TugasSeeder.php
+++ database/seeders/TugasSeeder.php
@@ -11,14 +11,18 @@ class TugasSeeder extends Seeder
     {
         DB::table('tugas')->insert([
             [
-                'title' => 'Tugas Perkenalan Diri',
-                'description' => 'Buat paragraf singkat perkenalan diri dalam bahasa Inggris.',
+                'judul' => 'Tugas Perkenalan Diri',
+                'deskripsi' => 'Buat paragraf singkat perkenalan diri dalam bahasa Inggris.',
+                'deadline' => now()->addDays(14),
+                'is_active' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'title' => 'Daily Activity Journal',
-                'description' => 'Tuliskan aktivitas harianmu dalam bahasa Inggris.',
+                'judul' => 'Daily Activity Journal',
+                'deskripsi' => 'Tuliskan aktivitas harianmu dalam bahasa Inggris.',
+                'deadline' => now()->addDays(14),
+                'is_active' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
